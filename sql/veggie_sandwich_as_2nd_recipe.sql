08297d8ff07b5149a0f02332c6641bab39045cb7
Justin <jstrat1.618@gmail.com>
2017-11-06 09:05:41 -0700

    Change meal_time in veggie_sandwich_as_2nd_recipe

--- sql/veggie_sandwich_as_2nd_recipe.sql
+++ sql/veggie_sandwich_as_2nd_recipe.sql
@@ -1,5 +1,5 @@
 INSERT INTO recipes(category_id, recipe_name, serving_size, meal_time, instructions)
-VALUES(6,'hummus veggie sandwich', 1, 'lunch or dinner', 'Spread the hummus on both sides of the bread. Slice the cucumber and tomatoe. Throw on cucumbers, tomatoes, shredded carrot and spinach.');
+VALUES(6,'hummus veggie sandwich', 1, 'lunch', 'Spread the hummus on both sides of the bread. Slice the cucumber and tomatoe. Throw on cucumbers, tomatoes, shredded carrot and spinach.');
 
 INSERT INTO ingredients(recipe_id, ingredient_name, amount, unit)
 VALUES
